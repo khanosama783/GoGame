b6ce50571060ffc46a7c7b06a0bcca6fd74c7ff5
khanosama783 <khanosama783@gmail.com>
2024-09-04 21:35:47 +0530

    idle player animation added

--- game.go
+++ game.go
@@ -63,18 +63,25 @@ func input() {
 
 func update() {
 	running = !rl.WindowShouldClose()
+
 	playerSrc.X = 0
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+
 	if playerMoving {
 		if playerUp { playerDest.Y -= playerSpeed }
 		if PlayerDown { playerDest.Y += playerSpeed }
 		if playerLeft { playerDest.X -= playerSpeed }
 		if playerRight { playerDest.X += playerSpeed }
 		if frameCount % 8 == 1 { playerFrame++ }
-		playerSrc.X = playerSrc.Width * float32(playerFrame)
+	} else if frameCount % 45 == 1 {
+		playerFrame++
 	}
 
 	frameCount++
 	if playerFrame > 3 { playerFrame = 0 }
+	if !playerMoving && playerFrame > 1 { playerFrame = 0 }
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
 	playerSrc.Y = playerSrc.Height * float32(playerDir)
 
 	rl.UpdateMusicStream(music)
